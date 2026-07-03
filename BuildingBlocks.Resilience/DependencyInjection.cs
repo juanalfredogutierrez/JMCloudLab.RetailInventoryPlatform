a65c1c9dcd63ab4e45e32d7cb4860fbba15f0884
juanalfredogutierrez <jgutierrez_pe@outlook.com>
2026-07-03 16:26:33 -0500

    feat(buildingblocks): add HTTP resilience support with Polly v8

--- BuildingBlocks.Resilience/DependencyInjection.cs
+++ BuildingBlocks.Resilience/DependencyInjection.cs
@@ -1,7 +1,14 @@
-﻿namespace BuildingBlocks.Resilience
-{
-    public class DependencyInjection
-    {
+﻿using BuildingBlocks.Resilience.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace BuildingBlocks.Resilience;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBuildingBlockResilience(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddResilience(configuration);
+        return services;
     }
 }
---
+++ BuildingBlocks.Resilience/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.Resilience.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Resilience.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddResilience(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ResilienceOptions>(configuration.GetSection(ResilienceOptions.SectionName));
+
+        return services;
+    }
+}
