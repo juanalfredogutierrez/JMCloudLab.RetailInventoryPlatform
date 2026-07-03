61080e8f08e1ba18b4260e4a9e3dd92017946c9f
juanalfredogutierrez <jgutierrez_pe@outlook.com>
2026-07-03 15:14:49 -0500

    feat(resilience): implement timeout strategy

--- BuildingBlocks.Resilience/Strategies/TimeoutStrategy.cs
+++ BuildingBlocks.Resilience/Strategies/TimeoutStrategy.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BuildingBlocks.Resilience.Options;
+using Microsoft.Extensions.Http.Resilience;
 
-namespace BuildingBlocks.Resilience.Strategies
+namespace BuildingBlocks.Resilience.Strategies;
+
+internal static class TimeoutStrategy
 {
-    internal class TimeoutStrategy
+    public static void Configure( HttpTimeoutStrategyOptions options,TimeoutOptions timeoutOptions)
     {
+        options.Timeout = TimeSpan.FromSeconds(timeoutOptions.TimeoutInSeconds);
     }
 }
