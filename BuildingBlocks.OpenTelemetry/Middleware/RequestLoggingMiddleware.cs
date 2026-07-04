6350dbf3ef39a2b8167d2f5156eb30665a1b3176
juanalfredogutierrez <jgutierrez_pe@outlook.com>
2026-07-04 07:17:15 -0500

    chore: centralize NuGet package versions using Directory.Packages.props

---
+++ BuildingBlocks.OpenTelemetry/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.OpenTelemetry.Middleware
+{
+    internal class RequestLoggingMiddleware
+    {
+    }
+}
